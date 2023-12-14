d7e21afdc7b3610ed568f1a8dee431b7c270a49c
Random <rc786986@gmail.com>
2023-12-14 13:14:42 +0530

    ChatApp ....

--- src/Routes/userRoutes.js
+++ src/Routes/userRoutes.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const addUserPage = require("../controler/userControler")
+const sendMessage = require("../controler/messageController")
 
 console.log(addUserPage)
+console.log(sendMessage)
 
 router.post("/addUser",addUserPage.addUser)
 router.get("/getUser",addUserPage.getUser)
 router.post("/login",addUserPage.login)
+
+router.get("/addMessage", sendMessage.addMessage)
+router.get("/getMessage", sendMessage.getMessage)
 
 
 
